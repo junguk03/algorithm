f2c7cd92394f8311c4bf6657e20e3acce09d5b33
junguk03 <97594871+junguk03@users.noreply.github.com>
2023-05-12 08:59:15 +0900

    measure
    char has a range of numbers from -127 to 128. I didn't take that into account and overflow occurred

--- measure_2.c
+++ measure_2.c
@@ -1,29 +1,26 @@
 #include<stdio.h>
-#include<string.h>
 
 int main(void){
-    int n, i, sum = 0, j = 0, length;
+    int n, i, sum = 0, j = 0, length, k;
     while(1){
-        char fix[10000] = {0, };
+        sum = 0;
+        j = 0;
+        int fix[100000] = {0, };
         scanf("%d",&n);
         if(n == -1) break;
         for(i=1;i<n;i++){
             if(n%i == 0){
-                fix[j] = i;
-                j++;
+                fix[j++] = i;
                 sum += i;
             }
         }
-        length = strlen(fix);
         if(sum == n){
             printf("%d = %d",n,fix[0]);
-            for(i=1;i<length;i++){
+            for(i=1;i<j;i++){
                 printf(" + %d",fix[i]);
            }
            printf("\n");
         }
         else printf("%d is NOT perfect.\n",n);
-        sum = 0;
-        j = 0;
     }
 }
