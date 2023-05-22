92e76d0ba764598db4c0ce0e3dca42a1086abba2
junguk <junkuk9503@gmail.com>
2023-05-22 16:38:07 +0900

    add_commit

--- measure.c
+++ measure.c
@@ -3,6 +3,7 @@
 int main(void){
     int n, k, i, sum = 0;
     scanf("%d%d",&n,&k);
+<<<<<<< HEAD
     while(1){
         for(i=1;i<=n;i++){
             if(n%i == 0){
@@ -19,4 +20,13 @@ int main(void){
         }
         else break;
     }
+=======
+    for(i=1;i<=n;i++){
+        if(n%i == 0){
+            sum++;
+            if(sum == k) printf("%d",i);
+        }
+    }
+    if(sum < k) printf("0");
+>>>>>>> 8bc7841 (first commit)
 }
