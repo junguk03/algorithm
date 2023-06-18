efaa0b4e488df77802a9281b5998cfe6c8ff1c53
junguk <junkuk9503@gmail.com>
2023-06-19 01:02:46 +0900

    push

--- measure.c
+++ measure.c
@@ -3,24 +3,6 @@
 int main(void){
     int n, k, i, sum = 0;
     scanf("%d%d",&n,&k);
-<<<<<<< HEAD
-    while(1){
-        for(i=1;i<=n;i++){
-            if(n%i == 0){
-                sum++;
-                if(sum == k){
-                    printf("%d",i);
-                }
-            }
-            else continue;
-        }
-        if(sum < k){
-            printf("0");
-            break;
-        }
-        else break;
-    }
-=======
     for(i=1;i<=n;i++){
         if(n%i == 0){
             sum++;
@@ -28,5 +10,4 @@ int main(void){
         }
     }
     if(sum < k) printf("0");
->>>>>>> 8bc7841 (first commit)
 }
--- stack.c
+++ stack.c
@@ -2,10 +2,10 @@
 #include<string.h>
 
 int stack[100001];
-int count = 0;
+static int count = 0;
 
-void push(int n){
-    stack[count] = n;
+void push(int num){
+    stack[count] = num;
     count++;
 }
 void pop(){
@@ -14,37 +14,37 @@ void pop(){
         printf("%d\n",stack[count]);
         stack[count] = 0;
     }
-    else printf("-1\n");
+    else printf("스택이 비어있음\n");
 }
 void size(){
     printf("%d\n",count);
 }
 void empty(){
-    if(count == 0) printf("1\n");
-    else printf("0\n");
+    if(count == 0) printf("스택이 비어있음\n");
+    else printf("스택이 비어있지 않음\n");
 }
 void top(){
     if(count != 0){
         printf("%d\n",stack[count - 1]);
     }
-    else printf("-1\n");
+    else printf("스택이 비어있음\n");
 }
 
 int main(void){
-    int n;
-    char number[10];
-    scanf("%d",&n);
+    int InputSize;
+    char function[10];
+    scanf("%d",&InputSize);
     int stack[100];
-    for(int i=0;i<n;i++){
-        scanf("%s",number);
-        if(strcmp(number,"push") == 0){
+    for(int i=0;i<InputSize;i++){
+        scanf("%s",function);
+        if(strcmp(function,"push") == 0){
             int num;
             scanf("%d",&num);
             push(num);
         }
-        else if(strcmp(number,"pop") == 0) pop();
-        else if(strcmp(number,"size") == 0) size();
-        else if(strcmp(number,"empty") == 0) empty();
-        else if(strcmp(number,"top") == 0) top();
+        else if(strcmp(function,"pop") == 0) pop();
+        else if(strcmp(function,"size") == 0) size();
+        else if(strcmp(function,"empty") == 0) empty();
+        else if(strcmp(function,"top") == 0) top();
     }
 }
