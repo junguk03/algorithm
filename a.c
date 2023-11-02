3c7929dddd4620b81e98eac08ff55112264f00cd
junguk <junkuk9503@gmail.com>
2023-11-02 20:40:21 +0900

    commit

---
+++ a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+long long solution();
+
+// sequence_len은 배열 sequence의 길이입니다.
+long long solution(int sequence[], size_t sequence_len) {
+    long long answer = 0;
+    int max = 1000;
+    return answer;
+}
+
+int main(void){
+    int sum = 0;
+    int sequence[500001] = {0};
+    scanf("%s",&sequence);
+    for(int i = 0;i<500000;i++){
+        while(sequence[i] != 0){
+            sum++;
+        }
+    }
+    printf("%d",sum);
+}
